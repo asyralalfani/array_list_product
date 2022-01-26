9724c5c12435f6980af5e919111d47194df4ffd6
Moch Asyral Difa Alfani <asyral777@gmail.com>
2022-01-26 16:02:51 +0700

    add product model

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import Model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Mie", 10));
+        products.add(new Product("Soap", 2));
+
+        products.forEach(product -> System.out.println("Name : " + product.getName() + ", Quantity : " + product.getQuantity()));
+    }
 }
